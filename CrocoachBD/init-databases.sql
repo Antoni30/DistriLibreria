704846c7c6f8af649d0968beaaf7e28a7e80e3e4
Antoni30 <artoapanta3@espe.edu.ec>
2025-08-04 08:20:37 -0500

    feat: creacion de dokerfiles

---
+++ CrocoachBD/init-databases.sql
@@ -0,0 +1,6 @@
+-- Script para inicializar las bases de datos necesarias para los microservicios
+CREATE DATABASE IF NOT EXISTS dbpublicaciones;
+CREATE DATABASE IF NOT EXISTS dbnotificaciones;
+CREATE DATABASE IF NOT EXISTS dbcatalogo;
+CREATE DATABASE IF NOT EXISTS bdautenticacion;
+CREATE DATABASE IF NOT EXISTS dbusuarios;
